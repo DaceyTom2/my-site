202ec2a90e4aca056dd8176a11e1eda83c93d309
Thomas Dacey <daceytom2@gmail.com>
2022-02-07 22:36:36 -0500

    cleaned up splash page

--- components/atoms/ButtonLink.js
+++ components/atoms/ButtonLink.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function ButtonLink(props) {
   return (
     <Link href={props.href}>
-      <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-1 rounded">
+      <a className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 m-2 rounded">
         {props.text}
       </a>
     </Link>
--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,11 @@ import ButtonLink from "../components/atoms/ButtonLink";
 
 export default function Splash() {
   return (
-    <div>
-      <div className="flex">
-        <div className="m-auto">
-          <h1 className="text-3xl font-bold underline p-5">Welcome Bonjour</h1>
-        </div>
-      </div>
-
-      <div className="flex">
-        <div className="m-auto">
+    <div className="flex h-screen bg-gradient-to-r from-gray-400 to-blue-300">
+      <div className="m-auto flex-col text-center">
+      <img class="object-cover h-72 w-96 rounded-lg" src="images/profile.jpg" alt="" />
+        <h1 className="text-3xl font-bold underline p-5">Welcome Bonjour</h1>
+        <div className="m-auto text-center">
           <ButtonLink href="/home" text="English" />
           <ButtonLink href="/fr/home" text="francais" />
         </div>
