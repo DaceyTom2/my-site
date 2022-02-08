9b4ba2defa37c2718f756ffab2fa3998496aa363
Thomas Dacey <daceytom2@gmail.com>
2022-02-08 13:08:14 -0500

    updated about content

--- pages/about.js
+++ pages/about.js
@@ -5,11 +5,11 @@ export default function About() {
     <div>
       <h1 className="text-2xl p-2 text-gray-700" >About</h1>
       <h2>Myself</h2>
-      <ImageCard header="Thomas" paragraph="test" image="images/TLogo.png"></ImageCard>
-      <h2>Pets</h2>
-      <ImageCard header="Mabel" paragraph="test" image="images/Mabel.png"></ImageCard>
-      <ImageCard header="Lou" paragraph="test" image="images/Lou.png"></ImageCard>
-      <ImageCard header="Stout" paragraph="test" image="images/Stout.png"></ImageCard>
+      <ImageCard header="Thomas" paragraph="Hello world! I'm a React web developer" image="images/Profile.png"></ImageCard>
+      <h2>Our Pets</h2>
+      <ImageCard header="Mabel" paragraph="Our charming cat Mabel comes from Halifax found on the side of the road. The first in the family at 4 years old" image="images/Mabel.png"></ImageCard>
+      <ImageCard header="Lou" paragraph="The mischevious Lou was found in the Jock river just outside of Ottawa. Last to join at 6 months old" image="images/Lou.png"></ImageCard>
+      <ImageCard header="Stout" paragraph="Stout the bulldozer come from the humane socienty. Second oldest at 3 years old" image="images/Stout.png"></ImageCard>
     </div>
   );
 }
