498fb72b7ee6e50dbb94ee4c0ee554f89ad67b95
Thomas Dacey <daceytom2@gmail.com>
2022-02-08 16:27:30 -0500

    moved burger down

--- components/molecule/NavBar.js
+++ components/molecule/NavBar.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function NavBar(props) {
   const [OpenNav, setOpenNav] = useState(false);
-
+    
   const handleClick = (e) => {
     setOpenNav(!OpenNav);
   };
@@ -17,7 +17,7 @@ export default function NavBar(props) {
       </div>
       <div
         className={[
-          "sm:h-12 w-full sm:w-auto absolute top-0 right-0 sm:top-auto sm:left-auto flex sm:flex bg-white border-x-2 border-b-2 border-gray-300 sm:border-0 sm:bg-transparent ",
+          "sm:h-12 w-full sm:w-auto absolute top-16 right-0 sm:top-auto sm:left-auto flex sm:flex bg-white border-x-2 border-b-2 border-gray-300 sm:border-0 sm:bg-transparent ",
           OpenNav ? "" : " hidden",
         ]}
       >
